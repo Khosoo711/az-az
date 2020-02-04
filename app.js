bb2cf1fbd1681876ff8a2b3fc175643fc62e2fce
Khosoo <Goddark942@gmail.com>
2020-02-04 16:44:25 +0800

    toglogchiin hold tovchiig darah"
    clear

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч нэгдүгээр тоглогч 0, хоёрдугаар тоглогч 1;
-var ActivePlayer = 1;
+var ActivePlayer = 0;
 
 // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -17,11 +17,33 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var dicedom = document.querySelector(".dice");
 dicedom.style.display = "none";
+
+// shoog shiddeg bolgoh...( event list ashiglav).........
 document.querySelector(".btn-roll").addEventListener("click", function() {
   var Dice = Math.floor(Math.random() * 6) + 1;
   dicedom.style.display = "block";
+  //   buusan shoonii toog  web dr gargah...
   dicedom.src = "dice-" + Dice + ".png";
   //   alert("Шоо буулаа:" + Dice);
+  if (Dice !== 1) {
+    roundScore = roundScore + Dice;
+    document.getElementById("current-" + ActivePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + ActivePlayer).textContent = 0;
+
+    ActivePlayer === 0 ? (ActivePlayer = 1) : (ActivePlayer = 0);
+
+    // ulaan tsegiig shiljuuleh
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    //   if(ActivePlayer === 0) {
+    //       ActivePlayer = 1;
+
+    //   }else { ActivePlayer = 0;}
+    diceDom.style.display = "none";
+  }
 });
 
-console.log("Шоо ингэж буулаа:" + Dice);
+// console.log("Шоо ингэж буулаа:" + Dice);
